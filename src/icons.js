15acd370fc8cc7a7a7f2fe1066b7bf10fb2be9bd
abdelkee <abdell.anejjar@gmail.com>
2021-09-07 18:24:43 -0500

    testing2

--- src/icons.js
+++ src/icons.js
@@ -13,8 +13,6 @@ function Icon({code}) {
 
 
 
-
-
 export function MoreIcon() {
     return (
        <Icon code={"M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"}/>
