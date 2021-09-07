c704d99415a1f4bcc701d67c64fa720fd8ee6c56
abdelkee <abdell.anejjar@gmail.com>
2021-09-07 13:04:15 -0500

    change api

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,28 +1,27 @@
 import axios from 'axios';
 
-const api_products = "http://localhost:5000/products/";
-const api_orders = "http://localhost:5000/orders/";
+const api = "https://akys-grocery.herokuapp.com/";
 
 
 export const productsApi = {
     getProducts: () => 
-                axios.get(api_products).then(res => res.data),
+                axios.get(api+'products').then(res => res.data),
     createProduct: (body) => 
-                axios.post(api_products, body).then(res => res.data),
+                axios.post(api+'products', body).then(res => res.data),
     updateProduct: (id, body) => 
-                axios.put(api_products+id, body).then(res => res.data),
+                axios.put(api+'products'+id, body).then(res => res.data),
     deleteProduct: (id) => 
-                axios.delete(api_products+id).then(res => res.data)
+                axios.delete(api+'products'+id).then(res => res.data)
                 
 };
 
 export const ordersApi = {
     getOrders: () => 
-                axios.get(api_orders).then(res => res.data),
+                axios.get(api+'orders').then(res => res.data),
     
     makeOrder: (body) => 
-                axios.post(api_orders, body).then(res => res.data),
+                axios.post(api+'orders', body).then(res => res.data),
 
     deleteOrder: (id) => 
-                axios.delete(api_orders+id).then(res => res.data),
+                axios.delete(api+'orders'+id).then(res => res.data),
 };
